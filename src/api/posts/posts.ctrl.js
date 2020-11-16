22a3508c0138b6e40fc42f55bbb813dde68b0752
hansol <kimhan0421@naver.com>
2020-11-17 01:33:36 +0900

    update node.js

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,7 +1,5 @@
 let postId = 1;
-// 초기데이터
 
-//배열 초기 데이터
 const posts = [
   {
     id: 1,
@@ -10,8 +8,8 @@ const posts = [
   },
 ];
 
-//포스트 작성
-exports.write = (stx) => {
+// 포스트 작성
+exports.write = (ctx) => {
   const { title, body } = ctx.request.body;
   postId += 1;
   const post = { id: postId, title, body };
@@ -19,26 +17,26 @@ exports.write = (stx) => {
   ctx.body = post;
 };
 
-//포스트 목록조회
+// 포스트 목록 조회
 exports.list = (ctx) => {
   ctx.body = posts;
 };
 
-//특정 포스트 조회
+// 특정 포스트 조회
 exports.read = (ctx) => {
   const { id } = ctx.params;
   const post = posts.find((p) => p.id.toString() === id);
   if (!post) {
     ctx.status = 404;
     ctx.body = {
-      message: '포스트가 존재하지 않습니다',
+      message: '포스트가 존재하지 않습니다.',
     };
     return;
   }
-  ctx.body = posts;
+  ctx.body = post;
 };
 
-//특정 포스트 삭제
+// 특정 포스트 제거
 
 exports.remove = (ctx) => {
   const { id } = ctx.params;
@@ -46,10 +44,46 @@ exports.remove = (ctx) => {
   if (index === -1) {
     ctx.status = 404;
     ctx.body = {
-      message: '포스트가 존재하지 않습니다',
+      message: '포스트가 존재하지 않습니다.',
     };
     return;
   }
-  post.splice(index, 1);
+  posts.splice(index, 1);
   ctx.status = 204;
+};
+
+// 포스트 수정
+exports.replace = (ctx) => {
+  const { id } = ctx.params;
+  const index = posts.findIndex((p) => p.id.toString() === id);
+  if (index === -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message: '포스트가 존재하지 않습니다.',
+    };
+    return;
+  }
+  posts[index] = {
+    id,
+    ...ctx.request.body,
+  };
+  ctx.body = posts[index];
+};
+
+// 포스트 특정 수정
+exports.update = (ctx) => {
+  const { id } = ctx.params;
+  const index = posts.findIndex((p) => p.id.toString() === id);
+  if (index === -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message: '포스트가 존재하지 않습니다.',
+    };
+    return;
+  }
+  posts[index] = {
+    ...posts[index],
+    ...ctx.request.body,
+  };
+  ctx.body = posts[index];
 };
--- src/index.js
+++ src/index.js
@@ -7,13 +7,12 @@ const api = require('./api');
 const app = new Koa();
 const router = new Router();
 
-router.use('/api', api.routes());//api 라우트 적용
+router.use('/api', api.routes());
 
 app.use(bodyParser());
 
-//app인스턴스에 라우터 적용
-app.use(router.routes()).use(router.allowedMethods())
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
-  console.log('Listening to port 4000');
+  console.log('listening to port 4000');
 });
